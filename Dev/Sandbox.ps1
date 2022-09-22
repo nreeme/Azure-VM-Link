73d6353155cabb82a599dc4adccfab2991468a52
nreeme <tkdreeme@gmail.com>
2022-09-22 14:24:37 -0600

    base class methods and constructors

--- Dev/Sandbox.ps1
+++ Dev/Sandbox.ps1
@@ -1,17 +1,54 @@
-$resource = @{
-    "Location"       = "EastUS2"
-    "resourceGroup"  = @{
-        "Name"   = "Kronos2"
-        "Return" = @{}
+class resource {
+    [string]$location
+    [array[]]$resourceGroup
+    [array[]]$networkSecurityGroup
+    [array[]]$virtualNetwork
+    hidden [object] $tempWorkSpace
+    resource($name, $location, $IPSPACE, $obsValue) {
+        $this.tempWorkSpace = @{"hiddenValue" = $obsValue }
+        $this.location = $location
+        $this.resourceGroup = @{
+            "Name"   = $name
+            "Return" = @{}
+        }
+        $this.networkSecurityGroup = @{
+            "name"          = "$($name)Security"
+            "resourceGroup" = $null
+        
+        }
+        $this.virtualNetwork = @{ 
+            "name"          = "$($name)Vnet" 
+            "addressPrefix" = "$($IPSPACE)/16" 
+            "subnetName"    = "Kronos2Subnet"
+            "subnetPrefix"  = "$($IPSPACE)/24"
+            "return"        = @{}
+            "hiddenArg"     = $obsValue
+        }
     }
-    "virtualNetwork" = @{ 
-        "name"           = "Kronos2Vnet" 
-        "addressPrefix" = "10.0.0.0/16" 
-        "subnetName"    = "Kronos2Subnet"
-        "subnetPrefix"  = "10.0.0.0/24"
-        "return"         = @{}
+    [string]get_location() {
+        return $this.location
+    }
+    [void] reset_location() {
+        $this.location = "EastUS2"
     }
 }
+####Remote Data source
+$seedName = "Kronos2"    
+$location = "BUTTWHOE"
+$NetworkAddrs = "10.0.0.0"
+####
+
+####Local Work
+$resource = [resource]::new($seedName, $location, $NetworkAddrs, $null)
+####
+
+#### View Values at locations
+$resource.get_location()
+$resource.reset_location()
+$resource.get_location()
+####
+
+
 $resource.resourceGroup.return = az group create `
     --location $resource.location `
     --name $resource.resourceGroup.name | ConvertFrom-Json -Depth 20
