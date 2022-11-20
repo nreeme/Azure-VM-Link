0e326a274143fc0fc611a5814ffe227439d0488c
nreeme <tkdreeme@gmail.com>
2022-11-20 16:45:36 -0700

    Updated methods added RG, VM create, & image

--- Dev/VmConstructorFounction.ps1
+++ Dev/VmConstructorFounction.ps1
@@ -1,10 +1,19 @@
 class ClassName {
     [string]$name
     [string]$location
+    [string]$resourceGroup
+    [string]$image
     [object]$return
-    ClassName ($opt01, $opt02) {
+    ClassName ($opt01, $opt02, $opt03) {
         $this.name = $opt01
         $this.location = (if ($null -ne $opt02) { $opt02 } else { "EastUS2" } )
+        $this.resourceGroup = @{
+            "name" = $opt03
+            "Return" = [PSCustomObject][ordered]@{}
+        }
+        $this.image = @{
+            "image" = "canonical:0001-com-ubuntu-server-focal:20_04-lts-gen2:latest"
+        }
         $this.return = @{
             "resourceGroup"        = "" 
             "networkSecurityGroup" = ""
@@ -15,21 +24,24 @@ class ClassName {
         #LOG IN
     }
     [Void] method01 () {
-        $this.return.resourcegroup = az group create `
+        $this.resourcegroup = az group create `
             --location $this.location `
             --name $this.name | ConvertFrom-Json -Depth 20
     }
     [Void] method01 () {s
-        $this.return.resourcegroup = az group create `
-            --location $this.location `
-            --name $this.name | ConvertFrom-Json -Depth 20
+        $this.return = az vm create `
+            --resource-group $this.resourceGroup `
+            --name $this.name  `
+            --image $this.image `
+            --admin-username $this.name `
+            --generate-ssh-keys | ConvertFrom-Json -Depth 20
     }
     [Void] method0 () {
         #LOG OUT
     }
 }
 
-$test = [ClassName]::new("Kronos", "WestUS2")
+$test = [ClassName]::new("Test", "WestUS2", "Test")
 
 
 [System.Collections.ArrayList]$FinalResult = @()
@@ -53,6 +65,10 @@ $Data = @(
 
 $Data | ForEach-Object {
     $object.method0()
+    $object.method01()
+    $object.method02()
+    $object.method03()
+    $object.method04()
     [void]$FinalResult.add($object.return)
 }
 
